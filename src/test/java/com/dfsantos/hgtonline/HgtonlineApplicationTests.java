64d0bcf295cd832355ff930000f9f9d9a3d4accd
Diego Ferreira dos Santos <santos.diegoferreira@gmail.com>
2025-06-08 15:57:40 -0300

    test: validate app name

--- src/test/java/com/dfsantos/hgtonline/HgtonlineApplicationTests.java
+++ src/test/java/com/dfsantos/hgtonline/HgtonlineApplicationTests.java
@@ -1,11 +1,19 @@
 package com.dfsantos.hgtonline;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HgtonlineApplicationTests {
 
+    @Autowired
+    private Environment environment;
+
     @Test
-    void contextLoads() {}
+    void contextLoads() {
+        assertEquals("hgtonline", environment.getProperty("spring.application.name"));
+    }
 }
